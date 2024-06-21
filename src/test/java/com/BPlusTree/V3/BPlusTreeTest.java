f54150b509750dca40ac124e2acbe1dcc64025b2
韦永志 <3262954152@qq.com>
2024-06-21 21:09:42 +0800

    V3 删除函数收尾, 测试案例编写

--- src/test/java/com/BPlusTree/V3/BPlusTreeTest.java
+++ src/test/java/com/BPlusTree/V3/BPlusTreeTest.java
@@ -191,7 +191,7 @@ public class BPlusTreeTest {
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             testMain(args);
         }
     }
